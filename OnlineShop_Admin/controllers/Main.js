ad69c6ec20c57029b7e3b28250e3bc39f800560d
Thanh Tung Nguyen <tungnt267@gmail.com>
2020-09-23 12:53:50 +0700

    add OnlineShop_Admin

---
+++ OnlineShop_Admin/controllers/Main.js
@@ -0,0 +1,450 @@
+const linkAPI = `https://5f54430fe5de110016d51eb9.mockapi.io/api/products`;
+let productList = [];
+let currentPage = 1;
+
+fetchProductList = () => {
+  axios({
+    url: linkAPI,
+    method: "GET",
+    data: null,
+  })
+    .then((res) => {
+      mapData(res.data);
+      renderProductList();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+renderProductList = (data) => {
+  data = data || productList;
+
+  // Pagination
+  paginationProduct(data);
+
+  const perPage = +document.getElementById("perPage").value;
+  let startOfPage = (currentPage - 1) * perPage;
+  let endOfPage = currentPage * perPage;
+  if (data.length < endOfPage) {
+    endOfPage = data.length;
+  }
+
+  // Render Product List
+  let htmlContent = "";
+  for (let i = startOfPage; i < endOfPage; i++) {
+    htmlContent += `
+      <tr>
+        <td class="product-img">
+          <img
+            class="product-img"
+            src="${data[i].image}"
+          />
+        </td>
+        <td>${data[i].id}</td>
+        <td class="td-capital">${data[i].name}</td>
+        <td>${data[i].description}</td>
+        <td>$${data[i].price}</td>
+        <td>${data[i].inventory}</td>
+        <td class="product-type td-capital">${data[i].type}</td>
+        <td>
+          <div class="btn-config">
+            <button class="btn btn-danger rounded-circle mr-1" 
+              onclick="deleteProduct('${data[i].id}')">
+              <i class="fa fa-trash"></i>
+            </button>
+            <button class="btn btn-info rounded-circle
+              id="edit_${data[i].id}"
+              onclick="editProduct('${data[i].id}')"
+              data-target="#myModal"
+              data-toggle="modal">
+              <i class="fa fa-pencil"></i>
+            </button>
+          </div>
+        </td>
+      </tr>
+    `;
+  }
+  document.getElementById("tbodyList").innerHTML = htmlContent;
+};
+
+// Pagination
+paginationProduct = (data) => {
+  let numOfPage = Math.ceil(
+    data.length / +document.getElementById("perPage").value
+  );
+  let htmlPagination = `
+    <li class="page-item page-prev" id="liPrev">
+      <button class="page-link page-switch" type=button id="goStart"
+        onclick="switchPage('goStart')">
+        <i class="fa fa-angle-double-left"></i>
+      </button>
+    </li>
+    <li class="page-item page-prev" id="liPrev">
+      <button class="page-link page-switch" type=button id="prevPage"
+        onclick="switchPage('prevPage')">
+        <i class="fa fa-angle-left"></i>
+      </button>
+    </li>
+    <li class="page-item active">
+      <button class="page-link page-current border-none" type=button"
+        id="page_${currentPage}"
+        onclick="switchPage('page_${currentPage}')">
+        ${currentPage}<span class="sr-only">(current)</span>
+      </button>
+    </li>
+    <li class="number-of-page">
+      <span>of ${numOfPage}</span>
+    </li>
+    <li class="page-item page-next" id="liNext">
+      <button class="page-link page-switch" type=button id="nextPage"
+        onclick="switchPage('nextPage')">
+        <i class="fa fa-angle-right"></i>
+      </button>
+    </li>
+    <li class="page-item page-next" id="liNext">
+      <button class="page-link page-switch" type=button id="goEnd"
+        onclick="switchPage('goEnd')">
+        <i class="fa fa-angle-double-right"></i>
+      </button>
+    </li>
+  `;
+  document.getElementById("ulPagination").innerHTML = htmlPagination;
+
+  // Set style for pagination
+  if (currentPage === 1) {
+    document.getElementById("prevPage").disabled = true;
+    document.getElementById("prevPage").style.backgroundColor = "#e9ecef";
+    document.getElementById("goStart").disabled = true;
+    document.getElementById("goStart").style.backgroundColor = "#e9ecef";
+  }
+  if (currentPage === numOfPage) {
+    document.getElementById("nextPage").disabled = true;
+    document.getElementById("nextPage").style.backgroundColor = "#e9ecef";
+    document.getElementById("goEnd").disabled = true;
+    document.getElementById("goEnd").style.backgroundColor = "#e9ecef";
+  }
+};
+
+// Switch Page
+switchPage = (idButton) => {
+  if (idButton === "prevPage") {
+    currentPage--;
+    renderProductList();
+  }
+  if (idButton === "nextPage") {
+    currentPage++;
+    renderProductList();
+  }
+  if (idButton === "goStart") {
+    currentPage = 1;
+    renderProductList();
+  }
+  if (idButton === "goEnd") {
+    currentPage = Math.ceil(
+      productList.length / +document.getElementById("perPage").value
+    );
+    renderProductList();
+  }
+  if (idButton === `page_${currentPage}`) {
+    renderProductList();
+  }
+};
+
+// Switch Number Of Item Per Page
+switchItemPage = () => {
+  productList = [];
+  currentPage = 1;
+  fetchProductList();
+};
+
+// Modal Handling
+document.getElementById("btnAddNew").addEventListener("click", () => {
+  document.getElementById("modalTitle").innerHTML = "Add New Product";
+  document.getElementById("btnHandle").innerHTML = `
+      <button type="button" class="btn btn-success" onclick="addProduct()">Add Product</button>
+      `;
+  document.getElementById("btnReset").click();
+});
+
+// Add New Product
+addProduct = () => {
+  const id = document.getElementById("idProduct").value;
+  const name = document.getElementById("name").value;
+  const image = document.getElementById("image").value;
+  const description = document.getElementById("description").value;
+  const price = document.getElementById("price").value;
+  const inventory = document.getElementById("inventory").value;
+  const rating = document.getElementById("rating").value;
+  const type = document.getElementById("type").value;
+
+  let newProduct = new Product(
+    id,
+    name,
+    image,
+    description,
+    price,
+    inventory,
+    rating,
+    type
+  );
+
+  axios({
+    url: linkAPI,
+    method: "POST",
+    data: newProduct,
+  })
+    .then((res) => {
+      productList = [];
+      fetchProductList();
+      document.getElementById("btnClose").click();
+      document.getElementById("btnReset").click();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// Delete Product
+deleteProduct = (id) => {
+  axios({
+    url: linkAPI + `/${id}`,
+    method: "DELETE",
+    data: null,
+  })
+    .then((res) => {
+      productList = [];
+      fetchProductList();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+// Edit Product
+editProduct = (id) => {
+  // Modal Handling
+  document.getElementById("modalTitle").innerHTML = "Edit Product";
+  document.getElementById("btnHandle").innerHTML = `
+      <button type="button" class="btn btn-success" onclick="updateProduct('${id}')">Save</button>
+      `;
+
+  axios({
+    url: linkAPI + `/${id}`,
+    method: "GET",
+    data: null,
+  })
+    .then((res) => {
+      document.getElementById("idProduct").value = res.data.id;
+      document.getElementById("name").value = res.data.name;
+      document.getElementById("image").value = res.data.image;
+      document.getElementById("description").value = res.data.description;
+      document.getElementById("price").value = res.data.price;
+      document.getElementById("inventory").value = res.data.inventory;
+      document.getElementById("rating").value = res.data.rating;
+      document.getElementById("type").value = res.data.type;
+
+      document.getElementById("idProduct").setAttribute("disabled", true);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+updateProduct = (id) => {
+  const idProduct = document.getElementById("idProduct").value;
+  const name = document.getElementById("name").value;
+  const image = document.getElementById("image").value;
+  const description = document.getElementById("description").value;
+  const price = document.getElementById("price").value;
+  const inventory = document.getElementById("inventory").value;
+  const rating = document.getElementById("rating").value;
+  const type = document.getElementById("type").value;
+
+  const newProduct = new Product(
+    idProduct,
+    name,
+    image,
+    description,
+    price,
+    inventory,
+    rating,
+    type
+  );
+
+  axios({
+    url: linkAPI + `/${id}`,
+    method: "PUT",
+    data: newProduct,
+  })
+    .then((res) => {
+      productList = [];
+      fetchProductList();
+      document.getElementById("btnClose").click();
+      document.getElementById("btnReset").click();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// Find product
+findProduct = () => {
+  let result = [];
+  // Input: keyword
+  let keyword = document.getElementById("txtSearch").value;
+
+  for (let product of productList) {
+    // Convert keyword
+    keyword = nonAccentVietnamese(keyword).trim();
+
+    const id = nonAccentVietnamese(product.id);
+    const productName = nonAccentVietnamese(product.name);
+    const description = nonAccentVietnamese(product.description);
+    const price = "$" + nonAccentVietnamese(product.price);
+    const inventory = nonAccentVietnamese(product.inventory);
+    const type = nonAccentVietnamese(product.type);
+
+    if (
+      id.indexOf(keyword) !== -1 ||
+      productName.indexOf(keyword) !== -1 ||
+      description.indexOf(keyword) !== -1 ||
+      price.indexOf(keyword) !== -1 ||
+      inventory.indexOf(keyword) !== -1 ||
+      type.indexOf(keyword) !== -1
+    ) {
+      result.push(product);
+    }
+  }
+  if (result.length === 0) {
+    document.getElementById("ulPagination").style.display = "none";
+  } else {
+    document.getElementById("ulPagination").style.display = "flex";
+  }
+  renderProductList(result);
+};
+
+// Sort by name
+sortProductName = (type) => {
+  if (type === 1) {
+    // Up
+    productList.sort((a, b) => {
+      if (
+        nonAccentVietnamese(a.name.toLowerCase()) <
+        nonAccentVietnamese(b.name.toLowerCase())
+      )
+        return -1;
+      if (
+        nonAccentVietnamese(a.name.toLowerCase()) >
+        nonAccentVietnamese(b.name.toLowerCase())
+      )
+        return 1;
+      return 0;
+    });
+  } else {
+    // Down
+    productList.sort((a, b) => {
+      if (
+        nonAccentVietnamese(a.name.toLowerCase()) >
+        nonAccentVietnamese(b.name.toLowerCase())
+      )
+        return -1;
+      if (
+        nonAccentVietnamese(a.name.toLowerCase()) <
+        nonAccentVietnamese(b.name.toLowerCase())
+      )
+        return 1;
+      return 0;
+    });
+  }
+};
+
+// Sort by price
+sortProductPrice = (type) => {
+  if (type === 1) {
+    // Up
+    productList.sort((a, b) => +a.price.toLowerCase() - +b.price.toLowerCase());
+  } else {
+    // Down
+    productList.sort((a, b) => +b.price.toLowerCase() - +a.price.toLowerCase());
+  }
+};
+
+// Sort name up
+document.getElementById("sortNameUp").addEventListener("click", () => {
+  document.getElementById("sortNameUp").style.display = "none";
+  document.getElementById("sortNameDown").style.display = "inline";
+  renderProductList(sortProductName(1));
+});
+
+// Sort name down
+document.getElementById("sortNameDown").addEventListener("click", () => {
+  document.getElementById("sortNameDown").style.display = "none";
+  document.getElementById("sortNameUp").style.display = "inline";
+  renderProductList(sortProductName(-1));
+});
+
+// Sort price up
+document.getElementById("sortPriceUp").addEventListener("click", () => {
+  document.getElementById("sortPriceUp").style.display = "none";
+  document.getElementById("sortPriceDown").style.display = "inline";
+  renderProductList(sortProductPrice(1));
+});
+
+// Sort price down
+document.getElementById("sortPriceDown").addEventListener("click", () => {
+  document.getElementById("sortPriceDown").style.display = "none";
+  document.getElementById("sortPriceUp").style.display = "inline";
+  renderProductList(sortProductPrice(-1));
+});
+
+mapData = (data) => {
+  for (let product of data) {
+    const newProduct = new Product(
+      product.id,
+      product.name,
+      product.image,
+      product.description,
+      product.price,
+      product.inventory,
+      product.rating,
+      product.type,
+      product.prefArray
+    );
+    productList.push(newProduct);
+  }
+};
+
+findById = (id) => {
+  for (let product of productList) {
+    if (product.id === id) {
+      return product;
+    }
+  }
+};
+
+findPosition = (id) => {
+  for (let i in productList) {
+    if (productList[i].id === id) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+// Converting standard Vietnamese Characters to non-accent ones
+nonAccentVietnamese = (str) => {
+  str = str.toLowerCase();
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  str = str.replace(/đ/g, "d");
+  // Some system encode vietnamese combining accent as individual utf-8 characters
+  str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // Huyền sắc hỏi ngã nặng
+  str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // Â, Ê, Ă, Ơ, Ư
+  return str;
+};
+
+fetchProductList();
---
+++ OnlineShop_Admin/models/Product.js
@@ -0,0 +1,12 @@
+class Product {
+  constructor(id, name, image, description, price, inventory, rating, type) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.description = description;
+    this.price = price;
+    this.inventory = inventory;
+    this.rating = rating;
+    this.type = type;
+  }
+}
